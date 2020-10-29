87d4837183b8da142794c32feed87095eb1cd333
Mateus <mateustrindade8@gmail.com>
2020-10-28 22:19:12 -0300

    Inclusão da toolbar.

--- app/src/main/java/com/example/snackbar/ui/ProfileActivity.kt
+++ app/src/main/java/com/example/snackbar/ui/ProfileActivity.kt
@@ -3,11 +3,33 @@ package com.example.snackbar.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.snackbar.R
+import kotlinx.android.synthetic.main.activity_profile.*
+
+private lateinit var navController: NavController
+private lateinit var appBarConfiguration: AppBarConfiguration
+
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        setSupportActionBar(profileToolbar)
+
+        navController = findNavController(R.id.nav_host_fragment_container)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
